e15237520510b18803dc56cd1831dfca9cb8f569
Ruvick <rudikov.web@ya.ru>
2024-07-14 15:19:06 +0300

    Стартовые файлы

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+	import Header from './components/Header.vue'
+	import Footer from './components/Footer.vue'
+</script>
+
+<template>
+
+	<Header/>
+
+		<main class="page">
+
+			<section class="page__about about">
+				<div class="__container">
+
+					<h1 class="title">Title</h1>
+
+				</div>
+			</section>
+
+		</main>
+
+		<Footer/>
+
+</template>
+
+<style lang="scss" scoped>
+
+.title {
+	font-size: 36px;
+}
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+
+	<footer class="footer">
+		<div class="footer__container">
+			Footer
+		</div>
+	</footer>
+
+</template>
+
+<style lang="scss" scoped>
+
+	.footer {
+		padding: 10px 0;
+	}
+
+</style>
