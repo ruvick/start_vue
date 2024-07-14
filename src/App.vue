193908033e9e2b852331a6d3a2b15adfb106abaf
Ruvick <rudikov.web@ya.ru>
2024-07-14 19:07:06 +0300

    Обнов

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <script setup>
-	import Header from './components/Header.vue'
-	import Footer from './components/Footer.vue'
+
+	import Header from '@/components/Header.vue'
+	import IndexPage from '@/pages/IndexPage.vue';
+	import Footer from '@/components/Footer.vue'
+	// @ - элиас 
+
+// 	defineOptions({
+//   name: 'App'
+// });
+
 </script>
 
 <template>
 
 	<Header/>
-
-		<main class="page">
-
-			<section class="page__about about">
-				<div class="__container">
-
-					<h1 class="title">Title</h1>
-
-				</div>
-			</section>
-
-		</main>
-
-		<Footer/>
+	<IndexPage/>
+	<Footer/>
 
 </template>
 
-<style lang="scss" scoped>
-
-.title {
-	font-size: 36px;
-}
+<style lang="scss">
 
 </style>
+
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+
+	<main class="page">
+
+		<section class="page__about about"> 
+			<div class="__container">
+
+				<h1 class="page__title title">Title</h1>
+
+			</div>
+		</section>
+
+	</main>
+
+</template>
+
+<style lang="scss" scoped>
+
+	.title	{
+		font-size: 36px;
+		font-weight: 700;
+		line-height: 1.3;
+	}
+
+</style>
